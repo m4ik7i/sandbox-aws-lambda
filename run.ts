ecdab50048d80040a86203abbde8649f11e8290a
m4ik7i <m4ik7i@gmail.com>
2020-04-11 05:04:23 +0900

    refactor: deno script

---
+++ run.ts
@@ -0,0 +1,10 @@
+const run = async (cmd: string): Promise<void> => {
+  const { success, code } = await Deno.run(
+    { cmd: ["sh", "-c", cmd] },
+  ).status();
+  if (!success) {
+    throw new Error(`${cmd} exit with code ${code}`);
+  }
+};
+
+export default run;
--- sandbox-aws-lambda-java/build.ts
+++ sandbox-aws-lambda-java/build.ts
@@ -1,13 +1,6 @@
 #!/usr/bin/env deno --allow-run
 
-const run = async (cmd: string): Promise<void> => {
-  const { success, code } = await Deno.run(
-    { cmd: ["sh", "-c", cmd] },
-  ).status();
-  if (!success) {
-    throw new Error(`${cmd} exit with code ${code}`);
-  }
-};
+import { run } from "./deps.ts";
 
 try {
   await run("./gradlew build --no-daemon");
